b8a559314382e7aff3ad0ff936085738fa0d4818
Didar ABdulkhaliq <didar0xalil@gmail.com>
2022-06-07 01:42:55 +0300

    projects css

--- src/components/projects/ProjectsComp.js
+++ src/components/projects/ProjectsComp.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "./projects.css";
 import { FaLink } from "react-icons/fa";
-
+import noproject from "../assets/noproject.png"
 export default class projects extends Component {
   render() {
     return (
@@ -10,7 +10,10 @@ export default class projects extends Component {
           <div className="projectss">
             <p className="largeprojectsstext">My Projects</p>
             <div className="projectboxs">
-              <div className="myprojects">
+              <div className="projectcard-1">  
+              <p className="projecttitle">My portfolio</p>
+              <p className="projectinfo">A simple portfolio for myself </p>
+
                 <div className="previewlink">
                   <a
                     className="projectlink"
@@ -21,72 +24,7 @@ export default class projects extends Component {
                   </a>
                 </div>
               </div>
-              <div className="myprojects">
-                <div className="previewlink">
-                  <a
-                    className="projectlink"
-                    href="https://www.didar.dev"
-                  >
-                    {" "}
-                    <FaLink />
-                  </a>
-                </div>
-              </div>
-              <div className="myprojects">
-                <div className="previewlink">
-                  <a
-                    className="projectlink"
-                    href="https://www.didar.dev"
-                  >
-                    {" "}
-                    <FaLink />
-                  </a>
-                </div>
-              </div>
-              <div className="myprojects">
-                <div className="previewlink">
-                  <a
-                    className="projectlink"
-                    href="https://www.didar.dev"
-                  >
-                    {" "}
-                    <FaLink />
-                  </a>
-                </div>
-              </div>
-              <div className="myprojects">
-                <div className="previewlink">
-                  <a
-                    className="projectlink"
-                    href="https://www.didar.dev"
-                  >
-                    {" "}
-                    <FaLink />
-                  </a>
-                </div>
-              </div>
-              <div className="myprojects">
-                <div className="previewlink">
-                  <a
-                    className="projectlink"
-                    href="https://www.didar.dev"
-                  >
-                    {" "}
-                    <FaLink />
-                  </a>
-                </div>
-              </div>
-              <div className="myprojects">
-                <div className="previewlink">
-                  <a
-                    className="projectlink"
-                    href="https://www.didar.dev"
-                  >
-                    {" "}
-                    <FaLink />
-                  </a>
-                </div>
-              </div>
+            
             </div>
           </div>
         </header>
